2e46a18a77ffc8c0f676f9c089fe675b3845c247
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-06 21:17:21 -0500

    feat: :sparkles: learning hooks with simulated data

--- ghitarla/src/App.jsx
+++ ghitarla/src/App.jsx
@@ -1,31 +1,66 @@
-import {useState, useEffect} from 'react'
+import { useState } from 'react'
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
+import { db } from './data/db'  
 
 function App() {
 
-    const [auth, setAuth] = useState(false)
+    /*
+        Statements: Cada statements es una instruccion para hacer algo 
 
-    useEffect(() => {
-        if (auth) {
-            console.log('Autenticado')
-        }
-    }, [auth])
+            - Creacion de variables 
+            - Codigo de condicionales con if 
+            - Lanzar errores con Throw new Error()
+            - Iterar con while o for   
+    */
 
-    setTimeout(() => {
-        setAuth(true)
-    }, 3000);
+    const [data, setData] = useState(db)
+
+    const [cart, setCart] = useState([])
+
+    function addToCart (item) {
+        const itemExists = cart.findIndex( (guitar) => guitar.id === item.id )
+
+        if(itemExists !== -1 ) {
+            const updateCart = [...cart]
+            updateCart[itemExists].quantity++
+            setCart(updateCart)
+        } else {
+            item.quantity = 1 
+            setCart([...cart, item]  )
+        } 
+    }
+
+    
 
   return (
+
+    /*
+        Expressions: Una expresion es algo que produce un valor 
+
+            - Ternarios 
+            - Utilizar un array Method que genere un nuevo array 
+            - .map que genera un nuevo array a diferencia de for each
+    */
+
     <>
 
-    <Header/>
+    <Header
+        cart={cart}
+    />
 
     <main className= "container-xl mt-5">
         <h2 className= "text-center">Nuestra Colección</h2>
 
         <div className= "row mt-5">
-            <Guitar/>
+            {data.map((guitar) => (
+                <Guitar
+                    key = {guitar.id}
+                    guitar = {guitar}
+                    addToCart = {addToCart}
+                />
+            ))}
+            
         </div>
     </main>
 
--- ghitarla/src/components/Guitar.jsx
+++ ghitarla/src/components/Guitar.jsx
@@ -1,16 +1,27 @@
-export default function Guitar(){
+export default function Guitar({ guitar, addToCart }){
+
+    const { id, name, image, description, price } = guitar;
+
+    
     return(
         <div className= "col-md-6 col-lg-4 my-4 row align-items-center">
                 <div className= "col-4">
-                    <img className= "img-fluid" src="./public/img/guitarra_01.jpg" alt="imagen guitarra" />
+                    <img className= "img-fluid" src={`/img/${image}.jpg`} alt="imagen guitarra" />
                 </div>
+
                 <div className= "col-8">
-                    <h3 className= "text-black fs-4 fw-bold text-uppercase">Lukather</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit quae labore odit magnam in autem nesciunt, amet deserunt</p>
-                    <p className= "fw-black text-primary fs-3">$299</p>
+                    <h3 className= "text-black fs-4 fw-bold text-uppercase">{name}</h3>
+
+                    <p>{description}</p>
+                    <p className= "fw-black text-primary fs-3">$ {price}</p>
                     <button 
-                        type="button"
-                        className= "btn btn-dark w-100"
+                        type = "button"
+                        className = "btn btn-dark w-100"
+                        /*
+                            Call back para que no mande a llamar la funcion en automatico si no que espera el evento  
+                            El segundo call back para no tener que pasar el state
+                        */
+                        onClick = {() => addToCart(guitar)} 
                     >Agregar al Carrito</button>
                 </div>
         </div>
