ceea6b29b9f703de61669be234f13c7b830220be
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-08-29 00:49:46 -0500

    feat: :sparkles: Add MenuItem component to main App and implement addItem functionality

---
+++ calculadora_propinas_react/src/App.tsx
@@ -0,0 +1,40 @@
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
+
+function App() {
+	const { addItem } = useOrder();
+
+	return (
+		<>
+			<header className="bg-teal-400 py-5">
+				<h1 className="text-center text-4xl font-black">
+					Calculadora de propinas y consumo
+				</h1>
+			</header>
+
+			{/* md = media querys */}
+			<main className="max-w-7xl mx-40 mt-20 grid md:grid-cols-2">
+				<div className="p-5">
+					<h2 className="text-4xl font-black">Menu</h2>
+
+					<div className="space-y-3 mt-10">
+						{menuItems.map((item) => (
+							<MenuItem 
+								key={item.id} 
+								item={item} 
+								addItem={addItem}
+							/>
+						))}
+					</div>
+				</div>
+
+				<div>
+					<h2>consumo</h2>
+				</div>
+			</main>
+		</>
+	);
+}
+
+export default App;
