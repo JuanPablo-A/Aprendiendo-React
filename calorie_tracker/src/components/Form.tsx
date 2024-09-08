bc24cf2a769c729f293853dac1e9549a80f1cf0f
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-09-07 21:03:45 -0500

    refactor: :sparkles: refactor: Refactor Form component and add form validation
    - Add form validation to ensure that the name field is not empty and the calories field is greater than zero.

--- calorie_tracker/src/components/Form.tsx
+++ calorie_tracker/src/components/Form.tsx
@@ -1,9 +1,64 @@
+import { useState, ChangeEvent, FormEvent } from "react"
+import { Activity } from "../types"
 import { categories } from "../data/categories"
 
 export default function Form() {
+
+    const [ activity, setActivity ] = useState<Activity>({
+        category: 1,
+        name: '',
+        calories: 0
+    }
+    )
+
+    /**
+     * Function for the mapping of the data, from the form to the Activity state
+     * e.target.id = Know what activity I am filling
+     * e.target.value = The value of the field I am filling
+     * + = converts e.target.value to number
+     * @param event 
+     */
+    const handleChange = ( event: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement> ) => {
+
+        /**
+         * Si estoy escribiendo en categoria o calorias retorna un TRUE 
+         */
+        const isNumberFill = ['category', 'calories'].includes(event.target.id)
+
+
+        setActivity({
+            ...activity,
+            [event.target.id]: isNumberFill ? +event.target.value : event.target.value  
+        })
+    }
+
+    /**
+     * Validation of the form fields
+     * name.trim() => join all spaces in the string
+     * @returns boolean
+     */
+    const isValidActivity = () => {
+        const { name, calories } = activity
+
+        return name.trim() !== '' && calories > 0 
+    }
+
+    /**
+     * Function for submitting the form
+     * @param e 
+     */
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        console.log('submit...')
+    }
+
+
+    
   return (
     <form 
         className="space-y-5 bg-white shadow p-10 rounded-lg"
+        onSubmit={handleSubmit}
     >
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Categorias</label>
@@ -11,6 +66,8 @@ export default function Form() {
             <select 
                 className="border border-slate-300 p-2 rounded-lg w-full bg-white" 
                 id="category"
+                value={activity.category}
+                onChange={ handleChange }
             >
                 { categories.map( category => (
                     <option 
@@ -23,12 +80,14 @@ export default function Form() {
             </select>
         </div>
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="activity" className="font-bold">Actividad</label>
+            <label htmlFor="name" className="font-bold">Actividad</label>
             <input 
                 type="text"
-                id="activity" 
+                id="name" 
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="EJ. comida, Jugo de naranja, ensalada, ejercicio, pesas, bicicleta "
+                value={activity.name}
+                onChange={handleChange}
             />
 
         </div>
@@ -39,13 +98,16 @@ export default function Form() {
                 id="calories" 
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="EJ. 300 o 500 "
+                value={activity.calories}
+                onChange={handleChange}
             />
         </div>
 
         <input 
             type="submit"
-            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-            value='Guardar comida o guardar ejercicio' 
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+            value={activity.category === 1 ? 'Guardar comida' : 'Guardar ejercicio'}
+            disabled = {!isValidActivity()}
         />
     </form>
   )
